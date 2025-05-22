4e7c36d99f4f3352ddeb3bb4709706a0914cf413
Emmelie83 <ej224sk@student.lnu.se>
2025-05-23 00:31:57 +0200

    Change tech stack font size on smaller screens

--- src/components/TimelineItem.tsx
+++ src/components/TimelineItem.tsx
@@ -19,19 +19,16 @@ const TimelineItem: React.FC<TimelineItemProps> = ({
 
 	return (
 		<div className="relative mb-6 pl-1 md:pl-3 lg:max-w-3/5 pt-3">
-			<div className="absolute left-[-1px] top-5 w-2 h-2 md:w-3 md:h-3 bg-[#585C64] rounded-full" />
+			<div className="absolute left-[-1px] top-5 w-2 h-2 md:w-3 md:h-3 bg-[#848a96] rounded-full" />
 			<div className="mx-3 md:mx-4">
-				<p className="font-bold">
-					{date}
-				</p>
+				<p className="font-bold">{date}</p>
 				<h4>{title}</h4>
-				<p className="italic mb-1">
-					{company}
-				</p>
+				<p className="italic mb-1">{company}</p>
 			</div>
-			
+
 			<div className="w-full ml-4">
-				<button id="plus-sign"
+				<button
+					id="plus-sign"
 					className={`leading-1 mb-4 transition-transform ${
 						isOpen ? "rotate-45" : ""
 					}`}
