51e5c5f95cde5a0dd6d9512ab048f1c96abfd0b8
Emmelie83 <ej224sk@student.lnu.se>
2025-06-20 15:01:49 +0200

    Make menu item underline on hover full width on mobile

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,26 +8,29 @@ export default function NavBar() {
 	const toggleMenu = () => setMenuOpen((prev) => !prev);
 
 	const navLinks = useCallback(
-		(onLinkClick?: () => void) =>
-			navSections.map((section) => (
-				<a
-					key={section}
-					href={`#${section}`}
-					className="hover:underline underline-offset-8"
-					onClick={onLinkClick}>
-					{section.charAt(0).toUpperCase() + section.slice(1)}
-				</a>
-			)),
-		[]
-	);
+    (onLinkClick?: () => void) =>
+      navSections.map((section) => (
+        <a
+          key={section}
+          href={`#${section}`}
+          onClick={onLinkClick}
+          className="relative w-full md:w-auto py-2 text-left md:text-center uppercase
+                             before:content-[''] before:absolute before:bottom-0 before:left-0
+                             before:h-[1px] before:w-0 before:bg-white before:transition-all before:duration-300
+                             hover:before:w-full">
+          {section.charAt(0).toUpperCase() + section.slice(1)}
+        </a>
+      )),
+    []
+  );
 
 	return (
 		<header className="flex justify-center">
 			<nav
 				id="navbar"
-				className="fixed text-md font-light top-0 w-full z-20 bg-[#10101A] text-white lg:hover:brightness-175 max-w-[1800px] p-2 shadow-md">
+				className="fixed text-md font-light top-0 w-full z-20 bg-[#10101A] text-white lg:hover:brightness-175 max-w-[1800px] p-2">
 				{/* Desktop */}
-				<div className="hidden md:flex px-10 py-3 gap-6 lg:gap-12 items-center justify-end uppercase">
+				<div className="hidden md:flex px-10 py-2 md:gap-8 lg:gap-12 items-center justify-end uppercase">
 					{navLinks()}
 				</div>
 
@@ -45,7 +48,7 @@ export default function NavBar() {
 				{/* Mobile menu */}
 				{menuOpen && (
 					<div
-						className="flex flex-col gap-8 px-10 pb-10 md:hidden bg-[#10101A] text-left uppercase items-start w-full h-lvh transition-all duration-300 ease-in-out"
+						className="flex flex-col gap-4 hover:border-b-white px-10 pb-10 md:hidden bg-[#10101A] text-left uppercase items-start w-full h-lvh transition-all duration-300 ease-in-out"
 						tabIndex={0}>
 						{navLinks(() => setMenuOpen(false))}
 					</div>
