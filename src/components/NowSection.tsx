5d297a0f23ffcbb4227f76cc3e17cffdda497308
Emmelie83 <ej224sk@student.lnu.se>
2025-06-26 23:08:55 +0200

    Add book that I am reading and format the section

--- src/components/NowSection.tsx
+++ src/components/NowSection.tsx
@@ -3,24 +3,33 @@ export default function NowSection() {
 		<div className="lg:p-6 max-w-[300px] md:max-w-[400px]">
 			<div className="space-y-6">
 				<div>
-					<p className="font-semibold flex lg:items-center gap-2 border-b-2 border-[#848a96] pb-2 mb-2">
+					<p className="font-semibold flex flex-col lg:flex-row lg:items-center gap-2 border-b-2 border-[#848a96] pb-2 mb-2">
 						Books I'm reading
 					</p>
-					<ul>
-						<li>Clean Code, Robert C. Martin</li>
+					<ul className="list-disc list-inside space-y-1">
+						<li>
+							<em>Clean Code</em>, Robert C. Martin
+						</li>
+						<li>
+							<em>
+								Android Programming — The Big Nerd Ranch Guide
+							</em>
+							, Sills et al.
+						</li>
 					</ul>
 				</div>
 				<div>
-					<p className="font-semibold flex lg:items-center gap-2 border-b-2 border-[#848a96] pb-2 mb-2">
+					<p className="font-semibold flex flex-col lg:flex-row lg:items-center gap-2 border-b-2 border-[#848a96] pb-2 mb-2">
 						Courses I'm taking
 					</p>
-					<ul>
+					<ul className="list-disc list-inside space-y-1">
 						<li>
-							The Complete Agentic AI Engineering Course (2025)
+							<em>The Complete Agentic AI Engineering Course</em>{" "}
+							(2025)
 						</li>
 						<li>
-							Android Mobile App Development (7.5 ETCS), Umeå
-							University
+							<em>Android Mobile App Development</em> (7.5 ECTS),
+							Umeå University
 						</li>
 					</ul>
 				</div>
