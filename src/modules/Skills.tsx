dbcf005c3b8b1c83726672e8bbf46875b2ab4328
Emmelie83 <ej224sk@student.lnu.se>
2025-07-12 21:25:41 +0200

    Add Android icon and change order of icons

--- src/modules/Skills.tsx
+++ src/modules/Skills.tsx
@@ -1,5 +1,6 @@
 import SkillItem from "../components/SkillItem";
 
+import androidStudioIcon from "../assets/icons/android.svg";
 import javaIcon from "../assets/icons/java.svg";
 import springbootIcon from "../assets/icons/springboot.svg";
 import mysqlIcon from "../assets/icons/mysql.svg";
@@ -8,8 +9,6 @@ import reactIcon from "../assets/icons/react.svg";
 import thymeleafIcon from "../assets/icons/thymeleaf.svg";
 import javascriptIcon from "../assets/icons/javascript.svg";
 import typescriptIcon from "../assets/icons/typescript.svg";
-import mavenIcon from "../assets/icons/maven.svg";
-import gradleIcon from "../assets/icons/gradle.svg";
 import vueIcon from "../assets/icons/vue.svg";
 import dockerIcon from "../assets/icons/docker.svg";
 import gitIcon from "../assets/icons/git.svg";
@@ -20,21 +19,22 @@ import linuxIcon from "../assets/icons/linux.svg";
 import figmaIcon from "../assets/icons/figma.svg";
 import tailwindIcon from "../assets/icons/tailwind.svg";
 import kotlinIcon from "../assets/icons/kotlin.svg";
+import springSecurityIcon from "../assets/icons/springsecurity.svg";
 
 const skills = [
 	[
 		{ icon: javaIcon, label: "Java" },
-		{ icon: springbootIcon, label: "Spring Boot" },
-		{ icon: thymeleafIcon, label: "Thymeleaf" },
-		{ icon: mysqlIcon, label: "MySQL" },
-		{ icon: mongoDBIcon, label: "MongoDB" },
-	],
-	[
-		{ icon: reactIcon, label: "React" },
-		{ icon: vueIcon, label: "Vue.js" },
-		{ icon: tailwindIcon, label: "Tailwind" },
+		{ icon: kotlinIcon, label: "Kotlin" },
 		{ icon: javascriptIcon, label: "JavaScript" },
 		{ icon: typescriptIcon, label: "TypeScript" },
+		{ icon: pythonIcon, label: "Python" },
+	],
+	[
+		{ icon: springbootIcon, label: "Spring Boot" },
+		{ icon: springSecurityIcon, label: "Spring Security" },
+		{ icon: androidStudioIcon, label: "Android Studio" },
+		{ icon: mysqlIcon, label: "MySQL" },
+		{ icon: mongoDBIcon, label: "MongoDB" },
 	],
 	[
 		{ icon: dockerIcon, label: "Docker" },
@@ -44,10 +44,10 @@ const skills = [
 		{ icon: linuxIcon, label: "Linux" },
 	],
 	[
-		{ icon: pythonIcon, label: "Python" },
-		{ icon: kotlinIcon, label: "Kotlin" },
-		{ icon: mavenIcon, label: "Maven" },
-		{ icon: gradleIcon, label: "Gradle" },
+		{ icon: reactIcon, label: "React" },
+		{ icon: vueIcon, label: "Vue.js" },
+		{ icon: tailwindIcon, label: "Tailwind" },
+		{ icon: thymeleafIcon, label: "Thymeleaf" },
 		{ icon: figmaIcon, label: "Figma" },
 	],
 ];
