67e7ef7f16dba91e877426eea4d2a38df2f24033
Emmelie83 <ej224sk@student.lnu.se>
2025-07-26 11:19:49 +0200

    Make nav div full width

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -38,10 +38,10 @@ export default function NavBar() {
 
 	return (
 		<header id="start" className="flex justify-center">
-			<nav className="fixed text-md font-light top-0 w-full z-20 bg-[#10101A] text-white max-w-[1800px] p-2">
+			<nav className="fixed text-md font-light top-0 w-full z-20 bg-[#10101A] uppercase">
 				{/* Desktop */}
-				<div className="hidden lg:block w-full hover:bg-[#1a1a2a] transition duration-300">
-					<div className="flex px-10 py-2 md:gap-8 items-center justify-end uppercase">
+				<div className="hidden lg:block w-full hover:bg-[#1a1a2a] transition duration-300 max-w-[1800px] mx-auto p-2">
+					<div className="flex px-10 py-2 md:gap-8 items-center justify-end">
 						<HomeLink />
 						<NavLinks
 							navSections={navSections}
@@ -58,13 +58,13 @@ export default function NavBar() {
 						onClick={toggleMenu}
 						aria-label="Toggle menu"
 						aria-expanded={menuOpen}>
-						{menuOpen ? <X size={40} /> : <Menu size={40} />}
+						{menuOpen ? <X size={35} /> : <Menu size={35} />}
 					</button>
 				</div>
 
 				{/* Mobile Menu */}
 				{menuOpen && (
-					<div className="flex flex-col gap-4 px-10 pb-10 lg:hidden bg-[#10101A] text-left uppercase items-start w-full h-lvh transition-all duration-300 ease-in-out">
+					<div className="flex flex-col gap-4 px-10 pb-10 lg:hidden bg-[#10101A] text-left items-start w-full h-lvh transition-all duration-300 ease-in-out">
 						<HomeLink onClick={() => setMenuOpen(false)} />
 						<NavLinks
 							navSections={navSections}
