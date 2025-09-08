f6d2ec507c646204ec15b8857aa503fe54875aa2
Emmelie83 <ej224sk@student.lnu.se>
2025-09-08 18:36:31 +0200

    Change about me text

--- src/modules/About.tsx
+++ src/modules/About.tsx
@@ -3,9 +3,8 @@ import linkedinIcon from "../assets/icons/linkedin.svg";
 import NowSection from "../components/NowSection";
 
 const paragraphs = [
-	"I’m a former translator and entrepreneur, and a lifelong learner.",
-	"Driven by curiosity, I moved from language and storytelling into tech, now building fullstack apps with Java, Spring Boot, TypeScript, and React.",
-	"Recently, I’ve been diving into AI, especially how it relates to language, communication, and the kind of translation work I once did.",
+	"I’m a former translator and entrepreneur who has transitioned into software engineering, specializing in building full-stack applications with Java, Spring Boot, and React.",
+	"As a lifelong learner, I’m always exploring new technologies and deepening my skills. Recently, I’ve been diving into AI, especially how it relates to language, communication, and the kind of translation work I once did.",
 	"When I’m not coding, you’ll find me bouldering, recharging in nature, or getting lost in a good book. I also love meeting new people, so feel free to reach out on LinkedIn or send me an email. I’d be glad to connect!",
 ];
 
